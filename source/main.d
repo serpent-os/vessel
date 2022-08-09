be5733912f95868d1a49bd68890945c5bfaf8381
Ikey Doherty <ikey@serpentos.com>
2022-08-09 02:02:30 +0100

    Initial build structure
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/main.d
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * main
+ *
+ * Main entry point into vessel
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
+
+module main;
+
+import vibe.d;
+
+/**
+ * Main entry point for vessel
+ *
+ * Params:
+ *      args = CLI arguments
+ * Returns: 0 if everything went to plan
+ */
+int main(string[] args)
+{
+    return runApplication();
+}
