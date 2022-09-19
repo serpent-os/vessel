c616aa98230f43b16e8e31f85b258b0f13fdc121
Ikey Doherty <ikey@serpentos.com>
2022-09-19 19:18:04 +0100

    models: Placeholder module for moss-db models
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/vessel/models/package.d
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2022 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * vessel.models
+ *
+ * Module level namespace imports
+ *
+ * Authors: Copyright © 2020-2022 Serpent OS Developers
+ * License: Zlib
+ */
