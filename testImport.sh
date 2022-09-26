3e81fb3fbbd2cb3524d33cb4c600c4c93fce3c09
Ikey Doherty <ikey@serpentos.com>
2022-09-26 21:08:15 +0100

    testImport: Actual usable assets
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- testImport.sh
+++ testImport.sh
@@ -2,4 +2,7 @@
 
 ## Testing imports
 curl -X POST localhost:5050/api/v1/collections/import -H "Content-Type: application/json" \
-    -d '{"reportID": 0, "collectables": [ {"type": "package", "uri": "https://dev.serpentos.com/protosnek/x86_64/bash-5.1.16-1-1-x86_64.stone", "sha256sum": "b171eef6dabe0f0db68a6def14fa541e1fb51d01e411a1f78c64735b62db2d22"}]}'
+    -d '{"reportID": 0, "collectables": [ 
+            {"type": "package", "uri": "https://dev.serpentos.com/protosnek/x86_64/lmdb-0.9.29-4-1-x86_64.stone",  "sha256sum": "a4aa4fa43ea41ffc5f4c92e457143a91cbacf0cbcbc941c63a98195c7f2aafd4"},
+            {"type": "package", "uri": "https://dev.serpentos.com/protosnek/x86_64/lmdb-devel-0.9.29-4-1-x86_64.stone",  "sha256sum": "7d15e42e9e1ba145acb849225a03dbc4a733b93358f5674a85b4a645a809e246"},
+        ]}'
