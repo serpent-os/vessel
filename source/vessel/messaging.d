75f18709602b5fa977d030f67cb47fc5057143cf
Ikey Doherty <ikey@serpentos.com>
2023-02-22 23:53:59 +0000

    messaging: Add new report channel + events
    We'll have to use this to avoid any sharing/threading issues with
    the ServiceContext and associated DBs to report events back through
    fibers with access to token management.
    
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

--- source/vessel/messaging.d
+++ source/vessel/messaging.d
@@ -21,6 +21,38 @@ public import taggedalgebraic.taggedalgebraic;
 public import std.stdint : uint64_t;
 import moss.service.models.endpoints;
 
+/** 
+ * Used to report success of an import
+ */
+public struct ReportSuccessEvent
+{
+    /** 
+     * Remote ID for reporting
+     */
+    uint64_t reportID;
+
+    /** 
+     * Who initiated the job
+     */
+    SummitEndpoint endpoint;
+}
+
+/** 
+ * Used to report failure of an import
+ */
+public struct ReportFailureEvent
+{
+    /** 
+     * Remote ID for reporting
+     */
+    uint64_t reportID;
+
+    /** 
+     * Who initiated the job
+     */
+    SummitEndpoint endpoint;
+}
+
 /**
  * A set of origins and index-matched hashes
  */
@@ -58,6 +90,22 @@ public union VesselEventSet
     ImportStonesEvent importStones;
 }
 
+/** 
+ * Reporting queue - handled in main vibe.d thread
+ */
+public union ReportEventSet
+{
+    /** 
+     * Send a failure report
+     */
+    ReportFailureEvent reportFailure;
+
+    /** 
+     * Send a success report
+     */
+    ReportSuccessEvent reportSuccess;
+}
+
 /**
  * An almost-sumtype of events
  */
@@ -68,3 +116,14 @@ public alias VesselEvent = TaggedAlgebraic!VesselEventSet;
  * algebraic type
  */
 public alias VesselEventQueue = Channel!(VesselEvent, 500);
+
+/** 
+ * All possible report events
+ */
+public alias ReportEvent = TaggedAlgebraic!ReportEventSet;
+
+/** 
+ * The service worker sends events back to the main vibe.d
+ * thread for async reporting to the main summit instance
+ */
+public alias ReportEventQueue = Channel!(ReportEvent, 500);
